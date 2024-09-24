df0e81cb9a4af39064323bc03cf536754223385f
Data708983 <2773277350@qq.com>
2024-09-24 21:29:53 +0800

    Menu Interaction 添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PICcs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Visibility = Visibility.Hidden;
+            Menu menu = new Menu(this);
+            menu.Show();
+        }
+    }
+}
---
+++ Menu.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PICcs
+{
+    /// <summary>
+    /// Menu.xaml 的交互逻辑
+    /// </summary>
+    public partial class Menu : Window
+    {
+        private MainWindow _mainWindow;
+
+        public Menu(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp|All Files|*.*",
+                Title = "选择图片文件"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                //MessageBox.Show("打开: " + openFileDialog.FileName);
+                _mainWindow.Title = "PIC " + openFileDialog.FileName;
+                _mainWindow.Visibility = Visibility.Visible;
+                Close();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://dataseven.hongshiite.cn/Help.html"; 
+            try
+            {
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("无法打开链接: " + ex.Message);
+            }
+        }
+    }
+}
