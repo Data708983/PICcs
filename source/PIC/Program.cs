c41e561439a673162ef2607fd63960129bfcdd38
Yoojun Zhou <hereisnotyoojun@outlook.com>
2024-09-26 21:55:17 +0800

    Optimize struct

---
+++ source/PIC/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIC
+{
+    public static class Program
+    {
+        public const string AppTitle = "PIC - Pulmonary Blood Vessels Cross-section Thickness Measurement";
+
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
